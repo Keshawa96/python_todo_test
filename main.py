35041f07cf9ba9fb272449240c2b10828b8bede5
Keshawa Methinda <kmethinda@gmail.com>
2024-12-31 23:32:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Different ways of doing this
+# type1 
+# print("Enter todo:")
+
+# type2
+# user_text = input("Enter todo:")
+# print(user_text)
+
+# type3
+user_prompt="Enter a todo :"
+user_text = input(user_prompt)
+print(user_text)
