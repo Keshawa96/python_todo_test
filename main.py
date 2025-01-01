fd1b328241c7586251d959e1eabe5a067a1b426e
Keshawa Methinda <kmethinda@gmail.com>
2025-01-01 10:26:34 +0530

    changed the toto list to a small application for testing .

--- main.py
+++ main.py
@@ -7,6 +7,19 @@
 # print(user_text)
 
 # type3
-user_prompt="Enter a todo :"
-user_text = input(user_prompt)
-print(user_text)
+# user_prompt="Enter a todo :"
+# user_text = input(user_prompt)
+# print(user_text)
+
+name = "Enter your name : "
+city = "Enter your city : "
+country = "Enter your country : "
+
+user_name = input(name)
+user_city = input(city)
+user_country = input(country)
+
+print(user_name )
+print(user_city)
+print(user_country)
+
