ad7358db1450d1a6bd59454011cb114c82b34e4b
Keshawa Methinda <kmethinda@gmail.com>
2025-01-10 17:05:13 +0530

    some while loop shit easy

---
+++ bonus2.1.py
@@ -0,0 +1,17 @@
+# x = 1 
+
+# while x <= 6:
+#     print(x)
+#     x = x + 1
+
+name = input("what is your name ? ")
+x = 1
+
+while x <= 6 :
+    print(name.capitalize())
+    x = x + 1
+
+     
+
+
+
